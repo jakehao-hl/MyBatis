7086e04ad59fd8ecc121cce1c2666767d355943f
Jake hao <jakehao@foxmail.com>
2020-03-31 17:04:25 +0800

    mybatis插件生成pojo类

---
+++ src/main/java/com/cn/dao/PersonMapper.java
@@ -0,0 +1,22 @@
+package com.cn.dao;
+
+import com.cn.pojo.Person;
+import com.cn.pojo.PersonExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface PersonMapper {
+    int countByExample(PersonExample example);
+
+    int deleteByExample(PersonExample example);
+
+    int insert(Person record);
+
+    int insertSelective(Person record);
+
+    List<Person> selectByExample(PersonExample example);
+
+    int updateByExampleSelective(@Param("record") Person record, @Param("example") PersonExample example);
+
+    int updateByExample(@Param("record") Person record, @Param("example") PersonExample example);
+}
